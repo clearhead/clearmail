eaed8103217c4cd259b892fd1863801c9f8de7f9
Brady Bryan <jbraden.bryan@gmail.com>
2015-08-21 11:58:31 -0400

    set up fallback endpoint for vitamix exp 2

--- node-email-endpoint/routes/index.js
+++ node-email-endpoint/routes/index.js
@@ -7,13 +7,13 @@ var connectionString = process.env.DATABASE_URL || 'postgres://aqnzjqbxjaqcya:a0
 router.get('/', function(req, res, next) {
   pg.connect(connectionString, function(err, client, done) {
     var query = client.query("SELECT * FROM users ORDER BY id ASC");
-  });
+  })
   res.render('index', { title: 'Clearhead Email Catcher' });
 });
 
 // post to homepage
 router.post('/', function(req, res, next) {
- res.send('Cross origin post allowed!');
+ res.send('Cross origin post allowed!')
 });
 
 router.post('/api/adduser', function(req, res) {
@@ -46,6 +46,33 @@ router.post('/api/adduser', function(req, res) {
         // Handle Errors
         if(err) {
           console.log(err);
+        }
+
+    });
+});
+
+// post to vitamix-exp-2 in case of error sending to vitamix backend
+router.post('/api/vitamix-2', function(req, res) {
+
+    // Grab data from http request
+    var data = {email: req.body.email, use: req.body.use};
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+
+        // SQL Query > Insert Data
+        client.query("INSERT INTO vitamixExp2 VALUES (country, userage, email) values($1, $2, $3)", [data.country, data.useage, data.email]);
+
+        // Handle Errors
+        if(err) {
+          res.send(err);
+          client.end();
+        }
+
+        // if no error send response
+        else {
+          res.send('Email successfully saved to clearhead email catcher');
+          client.end();
         }
 
     });
