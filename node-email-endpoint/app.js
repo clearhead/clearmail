37fbf00edde6762e4d1450329de18f60ba309ef4
Brady Bryan <jbraden.bryan@gmail.com>
2015-08-21 17:27:42 -0400

    adds vitamix-2 route

--- node-email-endpoint/app.js
+++ node-email-endpoint/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var vitamix2 = require('./routes/vitamix-2');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(function(req, res, next) {
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/vitamix-2', vitamix2);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
