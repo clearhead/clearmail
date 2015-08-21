d216399afe9c4d90dcaad7c1876385e1e599b7ca
Brady Bryan <jbraden.bryan@gmail.com>
2015-08-21 18:31:03 -0400

    updates database address, removes vitmix post path

--- node-email-endpoint/routes/index.js
+++ node-email-endpoint/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://aqnzjqbxjaqcya:a0y2Ij41opP2bfAbrCtyN2zfbg@ec2-54-197-255-248.compute-1.amazonaws.com:5432/d2ab9jp36cfls6';
+var connectionString = 'postgres://aqnzjqbxjaqcya:a0y2Ij41opP2bfAbrCtyN2zfbg@ec2-54-197-255-248.compute-1.amazonaws.com:5432/d2ab9jp36cfls6';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -49,35 +49,6 @@ router.post('/api/adduser', function(req, res) {
         }
 
     });
-});
-
-// post to vitamix-exp-2 in case of error sending to vitamix backend
-router.post('/api/vitamix-2', function(req, res) {
-
-    // Grab data from http request
-    var data = {country: req.body.country, usage: req.body.usage, email: req.body.email};
-
-    // Get a Postgres client from the connection pool
-    pg.connect(connectionString, function(err, client, done) {
-
-        // SQL Query > Insert Data
-        client.query("INSERT INTO vitamixExp2(country, useage, email) values($1, $2, $3)", [data.country, data.usage, data.email]);
-
-        // Handle Errors
-        if(err) {
-          res.send(err);
-          client.end();
-        }
-
-        // if no error send response
-        // else {
-        //   res.send('Email successfully saved to clearhead email catcher');
-        //   client.end();
-        // }
-
-    });
-
-  res.send('Email successfully saved to clearhead email catcher');
 });
 
 module.exports = router;
