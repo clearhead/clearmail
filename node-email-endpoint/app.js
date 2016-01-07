e341a172f0af162ac631a89b8d67797fc6838d09
Brady Bryan <jbraden.bryan@gmail.com>
2016-01-07 14:13:22 -0600

    removes all instances of vitamix route from app.js

--- node-email-endpoint/app.js
+++ node-email-endpoint/app.js
@@ -32,7 +32,7 @@ app.use(function(req, res, next) {
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/vitamix-2', vitamix2);
+// app.use('/vitamix-2', vitamix2);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
